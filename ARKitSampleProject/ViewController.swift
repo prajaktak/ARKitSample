b1d0f91bafd20029a04eab5055f855f764549391
Prajakta Kulkarni <prajakta.kulkarni@mirumagency.com>
2018-02-10 04:07:39 +0100

    Initial Commit

---
+++ ARKitSampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitSampleProject
+//
+//  Created by Prajakta Kulkarni on 2/10/18.
+//  Copyright © 2018 Prajakta Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
